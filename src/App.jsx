162d8a38b4a9754b93edb4e5509e90537c6bb9b1
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-23 16:29:53 +0200

    install_boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Main from "./components/layout/Main";
 
 export default function App() {
   return (
