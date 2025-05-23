8dc6f69142f33741550edf8083f6313908d1b3fc
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-23 16:52:41 +0200

    array_import

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Main from "./components/layout/Main";
+import languages from "./components/variables/languages";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Main languages={languages} />
       <Footer />
     </>
   );
